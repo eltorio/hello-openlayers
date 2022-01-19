83d4d9cbde787afafab9968a782b419a96319ff6
Ronan LE MEILLAT <ronan.le_meillat@sctg.eu.org>
2022-01-19 16:04:51 +0100

    first commit

--- src/main.js
+++ src/main.js
@@ -1,4 +1,37 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from "vue";
+import { createWebHistory, createRouter } from "vue-router";
 
-createApp(App).mount('#app')
+// styles
+
+import "@fortawesome/fontawesome-free/css/all.min.css";
+import "@/assets/styles/tailwind.css";
+
+// mouting point for the whole app
+
+import App from "@/App.vue";
+
+//vue3 openlayers
+import OpenLayersMap from 'vue3-openlayers';
+import 'vue3-openlayers/dist/vue3-openlayers.css';
+import Test from "@/views/OpenLayers.vue";
+
+// routes
+
+const routes = [
+  {
+    path: "/",
+    component: Test,
+  },
+  { path: "/:pathMatch(.*)*", redirect: "/" },
+];
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes,
+});
+
+const app = createApp(App);
+
+app.use(router);
+app.use(OpenLayersMap);
+app.mount("#app");
